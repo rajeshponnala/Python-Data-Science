77166d37f3a0cf43c9bf022fcbba7db2fc52039d
rajeshponnala <rajesh.ponnala@technoidentity.com>
2015-08-05 18:03:26 +0530

    add avg age to transformation table

--- MovieSimilarity.py
+++ MovieSimilarity.py
@@ -26,15 +26,11 @@ def getgenderPreference(g):
 def get_TransformedData(data):
     l = len(data['movie_id'].unique())
     z = sorted(data["movie_id"].unique())
-    df4= pd.DataFrame(np.zeros((l,5)),index=z,columns=['release_date','avg_rating','gender_preference','occupation','age'])
+    df4= pd.DataFrame(np.zeros((l,4)),index=z,columns=['release_date','avg_rating','gender_preference','age'])
     df4.index.name = 'Movie Id'
     df4['release_date'] = pd.cut(data["release_date"].map(lambda x: None if pd.isnull(x) else int(str(x).split('-')[-1])),bins=7,labels=['1922-1932','1933-1943','1944-1954','1955-1965','1966-1976','1977-1987','1988-1998'])
     df4['avg_rating'] = pd.cut(data.groupby('movie_id')['rating'].agg(np.mean),bins=5,labels=['R1','R2','R3','R4','R5'])
-   # df4['occupation'] = data['occupation'].map({'retired':'R', 'salesman':'S', 'engineer':'E', 'librarian':'LIB', 'student':'S', 'other':'O',
-    #   'executive':'EXE', 'administrator':'AD', 'artist':'A', 'entertainment':'ENT', 'educator':'EDU',
-     #  'scientist':'SCNT', 'none':'NA', 'programmer':'PRG', 'homemaker':'HM', 'marketing':'MKT',
-     #  'technician':'TEC', 'lawyer':'LAW', 'writer':'WRT', 'healthcare':'HC', 'doctor':'DOC'})
-    #df4['age']=pd.cut(data['age'],bins=[7,17,27,37,47,57,67,77],labels=['A1','A2','A3','A4','A5','A6','A7'])
+    df4['age']=pd.cut(data.groupby('movie_id')['age'].agg(np.mean),bins=[7,17,27,37,47,57,67,77],labels=['A1','A2','A3','A4','A5','A6','A7'])
     df4['gender_preference']= data.groupby('movie_id').apply(getgenderPreference)
     return df4
 
@@ -46,9 +42,12 @@ def getRank(movie1,movie2):
         rank+=1
     if movie1['gender_preference'] ==  movie2['gender_preference']:
         rank+=1
+    if movie1['age'] ==  movie2['age']:
+        rank+=1
     return rank
 
 def getSimilarityTable(data):
+    data = data.head(100)
     transformedData=get_TransformedData(data)
     l= len(data['movie_id'].unique())
     z = sorted(data["movie_id"].unique())
@@ -72,3 +71,8 @@ def refresh_data():
     movies_ratings = pd.merge(movies,ratings)
     lens = pd.merge(movies_ratings,users)
     get_movie_distance_table(lens).to_csv('moviedist.csv')
+
+#def userLikesOrDislikesMovie(user,movie):
+ #   data = lens.head(100)
+ #   tData = get_TransformedData(data)
+ #   simMovies = [ m for m in tData.ix[movie].index.drop(movie) if tData.ix[movie][m] == 4]
